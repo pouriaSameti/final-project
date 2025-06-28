3b95950588a451f3b68886a81a86612a62a84199
Pouria Sameti <pouria.sameti80@gmail.com>
2025-06-28 23:21:02 +0330

    CV -> Add main script to initialize ONVIF camera and display RTSP stream.

--- 3-Computer Vision & Line scan camera/main.py
+++ 3-Computer Vision & Line scan camera/main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from onvif_manager import ONVIFCameraManager
+from rtsp_player import RTSPVideoPlayer
+import cv2
 
 
-# Press the green button in the gutter to run the script.
+HOST = '192.168.1.1'
+PORT = 8899
+USERNAME = 'admin'
+PASSWORD = '@sdf1234'
+WSDL_DIR = 'C:\python-onvif-zeep\wsdl'
+WINDOW_NAME = 'Line Scan Camera'
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    camera_manager = ONVIFCameraManager(HOST, PORT, USERNAME, PASSWORD, WSDL_DIR)
+    camera_manager.connect()
+    rtsp_url = camera_manager.get_rtsp_url()
+    print("RTSP Stream URL:", rtsp_url)
+
+    cap = cv2.VideoCapture(rtsp_url)
+
+    if cap.isOpened():
+        print('Camera stream opened successfully.')
+        cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(WINDOW_NAME, 960, 540)
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Lost connection to the stream.")
+                break
+            cv2.imshow(WINDOW_NAME, frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+    else:
+        print('Failed to open camera stream.!!!')
