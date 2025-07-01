ea0d561ee99d106d813b71a7f92a06c20d0b5ef1
Pouria Sameti <pouria.sameti80@gmail.com>
2025-07-01 12:21:59 +0330

    CV -> implementation of Local segmentation with region_growing method.

--- 3-Computer Vision & Line scan camera/main.py
+++ 3-Computer Vision & Line scan camera/main.py
@@ -1,4 +1,5 @@
 from onvif_manager import ONVIFCameraManager
+from classic_segmentation import FrameSegmentation
 import cv2
 import numpy as np
 import time
@@ -13,7 +14,7 @@ WSDL_DIR = 'C:\\python-onvif-zeep\\wsdl'
 # Line Scan Parameters
 LINE_INDEX = 270
 MAX_LINES = 540
-SAMPLING_INTERVAL_SECONDS = 0.25  # one sample every 0.25 seconds (4 samples per second)
+SAMPLING_INTERVAL_SECONDS = 0.125  # one sample every 0.25 seconds (4 samples per second)
 WINDOW_NAME = "Line Scan Simulation"
 
 
@@ -54,7 +55,18 @@ if __name__ == '__main__':
                 if accumulated_image.shape[0] > MAX_LINES:
                     accumulated_image = accumulated_image[1:, :]
 
+                # show real-time image
+                # rotated = cv2.rotate(accumulated_image, cv2.ROTATE_90_CLOCKWISE)
+                # cv2.imshow(WINDOW_NAME, rotated)
+
+                # Local Segmentation with Utso
+                # rotated = cv2.rotate(accumulated_image, cv2.ROTATE_90_CLOCKWISE)
+                # segmented_image = FrameSegmentation.apply_local_segmentation(frame=rotated, segments=8)
+                # cv2.imshow(WINDOW_NAME, segmented_image)
+
+                # Segmentation with Region Growing
                 rotated = cv2.rotate(accumulated_image, cv2.ROTATE_90_CLOCKWISE)
+                segmented_mask = FrameSegmentation.apply_region_growing(rotated, threshold=128)
                 cv2.imshow(WINDOW_NAME, rotated)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
